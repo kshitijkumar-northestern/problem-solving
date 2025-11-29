674e2f74660c943623d0239dbdb9db9171a920cd
Kshitij Kumar <kumar.ks@northeastern.edu>
2025-11-28 22:20:58 -0500

    Create LeetCode Problem #2 - Add Two Numbers.py

---
+++ LeetCode Problem #2 - Add Two Numbers.py
@@ -0,0 +1,40 @@
+# https://leetcode.com/problems/add-two-numbers/
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        Add two numbers represented as linked lists in reverse order.
+        
+        :type l1: Optional[ListNode]
+        :type l2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode(0)
+        current = dummy
+        carry = 0
+        
+        while l1 or l2 or carry:
+            # Get values from current nodes, or 0 if None
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            
+            # Calculate sum and new carry
+            total = val1 + val2 + carry
+            carry = total // 10
+            digit = total % 10
+            
+            # Create new node with the digit
+            current.next = ListNode(digit)
+            current = current.next
+            
+            # Move to next nodes if they exist
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        
+        return dummy.next
